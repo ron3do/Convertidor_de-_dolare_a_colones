cec179ab59861ad8113b226b6b1368590891a21b
RONALD EDUARDO EDUARDO <>
2021-04-18 21:23:29 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char dolar=36; 
+  int cant;
+  int cambio=610;
+  float colones;
+  cout<< "digite la cantidad de dolares a convertir\n";
+  cin>>cant;
+  colones=cant*cambio;
+  cout<<" el equivalente en colones que digito es: \n"<<colones;
+
+}
